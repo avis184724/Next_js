fe5d9efc38b7d3fce41de56e5618ba37483ae33d
ckdtjqdld9632@naver.com <ckdtjqdld9632@naver.com>
2024-07-23 15:33:57 +0900

    7_23

---
+++ my-app/app/list/page.js
@@ -0,0 +1,38 @@
+'use client'
+import { useState } from "react"
+
+export default function List () {
+    let 상품 = ['Tomatoes', 'Pizza', 'Pasta'];
+    let [수량,수량변경] = useState([0,0,0]);
+    // 예시코드 -------------------------------
+
+    // let 어레이 = [2,3,4];
+    // 어레이.map((a,is) => {
+    //     return 10
+    // })
+
+    // ---------------------------------------
+
+    return (
+        <div>
+            <h4 className="title">상품 목록</h4>
+            {
+                상품.map((a, i) => {
+                    return (
+                        <div className="food" key={i}>
+                            <img src={`/food${i}.png`} className="food-img" alt="토마토"></img>
+                            <h4>{a} 14000원</h4>
+                            <span> {수량[i]}</span>
+                            <button onClick={() => {
+                                let copy = [...수량];
+                                copy[i]++;
+                                수량변경(copy);
+                            }}>+</button>
+                        </div>
+
+                    )
+                })
+            }
+        </div>
+    );  
+}
